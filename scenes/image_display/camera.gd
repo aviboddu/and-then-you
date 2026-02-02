378e6ae0741af0ae48df443cb3fe7b698027495a
Avinash Boddu <aviboddu@gmail.com>
2026-02-02 09:07:24 -0800

    Implemented simple parallax rendering and themes

--- scenes/image_display/camera.gd
+++ scenes/image_display/camera.gd
@@ -2,5 +2,4 @@ extends Camera2D
 
 func _process(_delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	var viewport_size = get_viewport_rect().size
-	self.position = mouse_pos / viewport_size
+	self.position = mouse_pos
---
+++ scenes/image_display/capture_theme.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export
+var foreground: Sprite2D
+@export
+var background: Sprite2D
+
+const foreground_color_name: StringName = "Accent"
+const background_color_name: StringName = "Background"
+const clear_color_name: StringName = "Clear"
+const theme_name: StringName = "Image"
+const shader_parameter_name: StringName = "color"
+
+var current_theme: Theme = load(ProjectSettings.get("gui/theme/custom")):
+	set(value):
+		apply_theme(value)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	apply_theme(current_theme)
+
+func apply_theme(value: Theme) -> void:
+	print(value.get_color(foreground_color_name, theme_name))
+	print(value.get_color(background_color_name, theme_name))
+	print(value.get_color(clear_color_name, theme_name))
+	foreground.set_instance_shader_parameter(shader_parameter_name, value.get_color(foreground_color_name, theme_name))
+	background.set_instance_shader_parameter(shader_parameter_name, value.get_color(background_color_name, theme_name))
+	RenderingServer.set_default_clear_color(value.get_color(clear_color_name, theme_name))
